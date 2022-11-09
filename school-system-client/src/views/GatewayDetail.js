92d5021e28c89f0ca3408a513d71f48e50ac8e92
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-11-09 23:53:46 +0100

    edit

--- school-system-client/src/views/GatewayDetail.js
+++ school-system-client/src/views/GatewayDetail.js
@@ -7,7 +7,6 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from "recharts";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -28,7 +27,8 @@ import {
   InputLabel,
   Select,
   MenuItem,
-  Alert,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 import {
   LocalizationProvider,
@@ -58,28 +58,29 @@ function createRow(name, value) {
 const STEP = 30;
 const DEFAULT_TIME = TIME_IN_MS.HOUR;
 
-function timerSetter(interval) {
+function timerSetter(interval, step) {
   const day = dayjs();
-  const roundedMinutes = Math.round(day.get("minute") / 10) * 10;
+  let roundedMinutes = 0;
   switch (interval) {
     case TIME_IN_MS.MINUTE:
-      return day.subtract(STEP + 1, "minute");
+      return day.subtract(step + 1, "minute");
     case TIME_IN_MS.FIVE_MINUTES:
-      return day
-        .subtract((STEP + 1) * 5, "minute")
-        .set("minute", roundedMinutes);
+      const d = day.subtract((step + 1) * 5, "minute");
+      roundedMinutes = Math.round(d.get("minute") / 10) * 10;
+      return d.set("minute", roundedMinutes);
     case TIME_IN_MS.TEN_MINUTES:
+      roundedMinutes = Math.round(day.get("minute") / 10) * 10;
       return day
-        .subtract((STEP + 1) * 10, "minute")
+        .subtract((step + 1) * 10, "minute")
         .set("minute", roundedMinutes);
     case TIME_IN_MS.HOUR:
-      return day.subtract(STEP + 1, "hour").set("minute", 0);
+      return day.subtract(step + 1, "hour").set("minute", 0);
     case TIME_IN_MS.DAY:
-      return day.subtract(STEP + 1, "day");
+      return day.subtract(step + 1, "day");
     case TIME_IN_MS.WEEK:
-      return day.subtract(STEP + 1, "week");
+      return day.subtract(step + 1, "week");
     case TIME_IN_MS.MONTH:
-      return day.subtract(STEP + 1, "month");
+      return day.subtract(step + 1, "month");
     default:
       return day;
   }
@@ -89,9 +90,10 @@ export const GatewayDetail = () => {
   const [state, setState] = useReducer(
     (prevState, currState) => ({ ...prevState, ...currState }),
     {
-      date: timerSetter(DEFAULT_TIME),
+      date: timerSetter(DEFAULT_TIME, STEP),
       interval: DEFAULT_TIME,
       limit: STEP,
+      displayDatetime: false,
     }
   );
   const { id } = useParams();
@@ -110,6 +112,7 @@ export const GatewayDetail = () => {
     return temperature?.data?.data?.map((temp) => ({
       temperature: temp.temperatureAvg,
       datetime: dayjs(temp.date).format("HH:mm:ss DD.MM.YYYY"),
+      date: dayjs(temp.date),
     }));
   }, [temperature]);
 
@@ -117,20 +120,21 @@ export const GatewayDetail = () => {
     return humidity?.data?.data?.map((hum) => ({
       humidity: hum.humidityAvg,
       datetime: dayjs(hum.date).format("HH:mm:ss DD.MM.YYYY"),
+      date: dayjs(hum.date),
     }));
   }, [humidity]);
 
   const rows = useMemo(() => {
     return [
-      createRow("Maximum temperature", `${temperature?.data?.max}°C` ?? "N/A"),
-      createRow("Minimum temperature", `${temperature?.data?.min}°C` ?? "N/A"),
+      createRow("Maximum temperature", `${temperature?.data?.max ?? "N/A"}°C`),
+      createRow("Minimum temperature", `${temperature?.data?.min ?? "N/A"}°C`),
       createRow(
         "Average temperature",
-        `${temperature?.data?.average}°C` ?? "N/A"
+        `${temperature?.data?.average ?? "N/A"}°C`
       ),
-      createRow("Maximum humidity", `${humidity?.data?.max}%` ?? "N/A"),
-      createRow("Minimum humidity", `${humidity?.data?.min}%` ?? "N/A"),
-      createRow("Average humidity", `${humidity?.data?.average}%` ?? "N/A"),
+      createRow("Maximum humidity", `${humidity?.data?.max ?? "N/A"}%`),
+      createRow("Minimum humidity", `${humidity?.data?.min ?? "N/A"}%`),
+      createRow("Average humidity", `${humidity?.data?.average ?? "N/A"}%`),
     ];
   }, [temperature, humidity]);
 
@@ -147,71 +151,117 @@ export const GatewayDetail = () => {
       <Grid container>
         <Grid item xs={12}>
           <Box display="flex">
+            <Box mr="0.5rem">
+              <FormControl>
+                <InputLabel>Interval</InputLabel>
+                <Select
+                  value={state.interval}
+                  label="Interval"
+                  onChange={(val) => {
+                    setState({
+                      interval: val.target.value,
+                      date: state.displayDatetime
+                        ? state.date
+                        : timerSetter(val.target.value, state.limit),
+                    });
+                  }}
+                >
+                  <MenuItem value={TIME_IN_MS.MINUTE}>1 Minute</MenuItem>
+                  <MenuItem value={TIME_IN_MS.FIVE_MINUTES}>5 Minutes</MenuItem>
+                  <MenuItem value={TIME_IN_MS.TEN_MINUTES}>10 Minutes</MenuItem>
+                  <MenuItem value={TIME_IN_MS.HOUR}>1 Hour</MenuItem>
+                  <MenuItem value={TIME_IN_MS.DAY}>1 Day</MenuItem>
+                  <MenuItem value={TIME_IN_MS.WEEK}>1 Week</MenuItem>
+                  <MenuItem value={TIME_IN_MS.MONTH}>1 Month</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box mr="0.5rem">
+              <FormControl>
+                <InputLabel>Step</InputLabel>
+                <Select
+                  value={state.limit}
+                  label="Step"
+                  onChange={(val) => {
+                    setState({
+                      limit: val.target.value,
+                      date: state.displayDatetime
+                        ? state.date
+                        : timerSetter(state.interval, val.target.value),
+                    });
+                  }}
+                >
+                  <MenuItem value={5}>5 records</MenuItem>
+                  <MenuItem value={10}>10 records</MenuItem>
+                  <MenuItem value={20}>20 records</MenuItem>
+                  <MenuItem value={30}>30 records</MenuItem>
+                  <MenuItem value={50}>50 records</MenuItem>
+                  <MenuItem value={100}>100 records</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          <Box>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.displayDatetime}
+                  onChange={(e) => {
+                    setState({
+                      displayDatetime: e.target.checked,
+                      date: e.target.checked
+                        ? state.date
+                        : timerSetter(state.interval, state.limit),
+                    });
+                  }}
+                />
+              }
+              label="Set date and time"
+            />
+          </Box>
+          {state.displayDatetime && (
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Box mr="0.5rem">
-                <FormControl>
-                  <InputLabel>Interval</InputLabel>
-                  <Select
-                    value={state.interval}
-                    label="Interval"
-                    onChange={(val) => {
-                      setState({
-                        interval: val.target.value,
-                        date: timerSetter(val.target.value),
-                      });
+              <Box display="flex">
+                <Box mr="0.5rem">
+                  <MobileDatePicker
+                    renderInput={(props) => <TextField disabled {...props} />}
+                    label="Date"
+                    inputFormat="DD.MM.YYYY"
+                    value={state.date}
+                    onChange={(newValue) => {
+                      setState({ date: newValue });
                     }}
-                  >
-                    <MenuItem defaultChecked value={TIME_IN_MS.MINUTE}>
-                      1 Minute
-                    </MenuItem>
-                    <MenuItem value={TIME_IN_MS.FIVE_MINUTES}>
-                      5 Minutes
-                    </MenuItem>
-                    <MenuItem value={TIME_IN_MS.TEN_MINUTES}>
-                      10 Minutes
-                    </MenuItem>
-                    <MenuItem value={TIME_IN_MS.HOUR}>1 Hour</MenuItem>
-                    <MenuItem value={TIME_IN_MS.DAY}>1 Day</MenuItem>
-                    <MenuItem value={TIME_IN_MS.WEEK}>1 Week</MenuItem>
-                    <MenuItem value={TIME_IN_MS.MONTH}>1 Month</MenuItem>
-                  </Select>
-                </FormControl>
+                  />
+                </Box>
+                {state.interval < TIME_IN_MS.DAY && (
+                  <Box mr="0.5rem">
+                    <TimePicker
+                      label="Time"
+                      value={state.date}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          onKeyDown={(e) => {
+                            e.preventDefault();
+                          }}
+                        />
+                      )}
+                      onChange={(newValue) => {
+                        setState({ date: newValue });
+                      }}
+                      shouldDisableTime={
+                        state.interval > TIME_IN_MS.TEN_MINUTES
+                          ? (timeValue, clockType) => {
+                              return clockType === "minutes" && timeValue > 0;
+                            }
+                          : undefined
+                      }
+                    />
+                  </Box>
+                )}
               </Box>
-              <Box mr="0.5rem">
-                <MobileDatePicker
-                  renderInput={(props) => <TextField disabled {...props} />}
-                  label="Date"
-                  inputFormat="DD.MM.YYYY"
-                  value={state.date}
-                  onChange={(newValue) => {
-                    setState({ date: newValue });
-                  }}
-                />
-              </Box>
-              {state.interval < TIME_IN_MS.DAY && (
-                <TimePicker
-                  label="Time"
-                  value={state.date}
-                  onChange={(newValue) => {
-                    setState({ date: newValue });
-                  }}
-                  shouldDisableTime={
-                    state.interval > TIME_IN_MS.TEN_MINUTES
-                      ? (timeValue, clockType) => {
-                          return clockType === "minutes" && timeValue > 0;
-                        }
-                      : undefined
-                  }
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              )}
             </LocalizationProvider>
-          </Box>
-          <Box my={"0.5rem"}>
-            <Alert severity="info">
-              Changing interval will also reset date and time
-            </Alert>
-          </Box>
+          )}
         </Grid>
         <Grid my={"2rem"} item xs={12}>
           <TableContainer component={Paper}>
@@ -262,10 +312,15 @@ export const GatewayDetail = () => {
               />
               <Tooltip />
               <Line
+                activeDot={{
+                  onClick: (e, { payload: { date } }) => {
+                    setState({ date, displayDatetime: true });
+                  },
+                  r: 8,
+                }}
                 type="monotone"
                 dataKey="humidity"
                 stroke="#8884d8"
-                activeDot={{ r: 8 }}
               />
             </LineChart>
           </ResponsiveContainer>
@@ -293,7 +348,16 @@ export const GatewayDetail = () => {
                 domain={["dataMin - 2", "dataMax + 2"]}
               />
               <Tooltip />
-              <Line type="monotone" dataKey="temperature" stroke="#82ca9d" />
+              <Line
+                activeDot={{
+                  onClick: (e, { payload: { date } }) => {
+                    setState({ date, displayDatetime: true });
+                  },
+                }}
+                type="monotone"
+                dataKey="temperature"
+                stroke="#82ca9d"
+              />
             </LineChart>
           </ResponsiveContainer>
         </Grid>
